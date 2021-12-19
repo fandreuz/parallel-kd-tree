d349ca40b781ee8ccaa3ae733dbda6c55d443f32
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-19 09:34:25 +0100

    define MPI data types

--- src/tree.h
+++ src/tree.h
@@ -1,17 +1,29 @@
+#ifdef MPI_VERSION
+#include <mpi.h>
+#endif
+
 #define NDIM 2
 
 #if !defined(DOUBLE_PRECISION)
 #define data_type float
+
+#ifdef MPI_VERSION
+#define mpi_data_type MPI_FLOAT
+#endif
+
 #else
 #define data_type double
+
+#ifdef MPI_VERSION
+#define mpi_data_type MPI_FLOAT
+#endif
+
 #endif
 
 // representation of an n-dimensional data point
 typedef kpoint data_type[NDIM];
 
 struct KNode {
-    // index of the dimension used to split the current branch in this node
-    int axis;
     // point used to split the tree
     kpoint data;
     // left and right branch originating from this node
