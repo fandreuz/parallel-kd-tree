a29105c1bc6df25f36f53d67597594998d688a95
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-21 16:30:25 +0100

    debug: show initial vector

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@ int main(int argc, char **argv) {
   data_type dt[SIZE];
   for (int i = 0; i < SIZE; i++) {
     dt[i] = i * i - 2 * i;
+
+#ifdef DEBUG
+    std::cout << i << " -> " << dt[i] << std::endl;
+#endif
   }
 
   data_type *tree = generate_kd_tree(dt, SIZE, DIMS);
