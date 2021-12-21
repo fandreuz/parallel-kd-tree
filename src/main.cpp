30ef1e267c576dd78f538f1a9bd86c31870c0050
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-21 16:22:39 +0100

    code cleanup. reduce number of items to facilitate debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include "tree_mpi.h"
 #include <iostream>
 
+#define SIZE 6
+#define DIMS 2
+
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
 
-  data_type dt[100];
-  for (int i = 0; i < 100; i++) {
+  data_type dt[SIZE];
+  for (int i = 0; i < SIZE; i++) {
     dt[i] = i * i - 2 * i;
   }
 
-  data_type *tree = generate_kd_tree(dt, 100, 2);
+  data_type *tree = generate_kd_tree(dt, SIZE, DIMS);
 
   MPI_Finalize();
 }
