2298c1ddfeff41e4b769c14e4f32ae5486831cd4
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-21 21:38:49 +0100

    debug result

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,39 @@
 #define SIZE 6
 #define DIMS 2
 
+void print(data_type *tree);
+
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
 
-  data_type dt[SIZE];
-  for (int i = 0; i < SIZE; i++) {
-    dt[i] = i * i - 2 * i;
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  data_type *dt = nullptr;
+  if (rank == 0) {
+    dt = new data_type[SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+      dt[i] = i * i - 2 * i;
 
 #ifdef DEBUG
-    std::cout << i << " -> " << dt[i] << std::endl;
+      std::cout << i << " -> " << dt[i] << std::endl;
 #endif
+    }
   }
 
+  delete[] dt;
+
   data_type *tree = generate_kd_tree(dt, SIZE, DIMS);
+  for (int i = 0; i < SIZE / DIMS; i++) {
+    std::cout << "(";
+    for (int j = 0; j < DIMS; j++) {
+      if (j > 0)
+        std::cout << ",";
+      std::cout << tree[i * DIMS + j];
+    }
+    std::cout << ")";
+  }
 
   MPI_Finalize();
 }
