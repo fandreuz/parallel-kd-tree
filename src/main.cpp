a631526b69fd29d952a574bd666608b2f2cfb29d
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-21 09:16:28 +0100

    add missing MPI compulsory function calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "tree_mpi.h"
 #include <iostream>
 
-int main() {
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+
   data_type dt[100];
   for (int i = 0; i < 100; i++) {
     dt[i] = i * i - 2 * i;
   }
 
   data_type *tree = generate_kd_tree(dt, 100, 2);
+
+  MPI_Finalize();
 }
