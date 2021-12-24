86b6014f9237bc2010ba0d632c1189b7d1bffec3
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2021-12-24 18:42:14 +0100

    improve pretty printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "tree_mpi.h"
 #include <iostream>
+#include <limits>
 
 #define SIZE 6
 #define DIMS 2
 
-void print(data_type *tree);
+void print(data_type *tree, int size);
 
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
@@ -30,40 +31,45 @@ int main(int argc, char **argv) {
 
   int size = SIZE;
   data_type *tree = generate_kd_tree(dt, size, DIMS);
-  if (rank == 0)
-    delete[] dt;
 
-  int next_powersum = 1;
-  int current_multiplier = 1;
-  int counter = 0;
   if (rank == 0) {
-    for (int i = 0; i < size; i++) {
-      std::cout << "(";
-      for (int j = 0; j < DIMS; j++) {
-        if (j > 0)
-          std::cout << ",";
-        std::cout << tree[i * DIMS + j];
-      }
-      std::cout << ")";
-
-      if (i + 1 == next_powersum) {
-        std::cout << "--";
-        current_multiplier *= 2;
-        next_powersum += current_multiplier;
-        counter = 0;
-      } else {
-        counter++;
-        if (counter == 2) {
-          std::cout << " | ";
-          counter = 0;
-        }
-      }
-    }
-  }
-
-  if(rank == 0) {
+    delete[] dt;
+    print(tree, size);
     delete[] tree;
   }
 
   MPI_Finalize();
+}
+
+void print(data_type *tree, int size) {
+  int next_powersum = 1;
+  int current_multiplier = 1;
+  int counter = 0;
+  for (int i = 0; i < size; i++) {
+    std::cout << "(";
+    for (int j = 0; j < DIMS; j++) {
+      if (j > 0)
+        std::cout << ",";
+
+      if (tree[i * DIMS + j] == std::numeric_limits<int>::min()) {
+        std::cout << "n/a";
+        break;
+      } else
+        std::cout << tree[i * DIMS + j];
+    }
+    std::cout << ")";
+
+    if (i + 1 == next_powersum) {
+      std::cout << std::endl;
+      current_multiplier *= 2;
+      next_powersum += current_multiplier;
+      counter = 0;
+    } else {
+      counter++;
+      if (counter == 2) {
+        std::cout << " | ";
+        counter = 0;
+      }
+    }
+  }
 }
