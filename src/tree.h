fd56f635d0f9c2a597b440ddaee2a66fdf580724
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-01-05 17:26:58 +0100

    define MPI types only if MPI is used in compilation

--- src/tree.h
+++ src/tree.h
@@ -19,11 +19,14 @@
  * @brief A function used to convert from string to `data_type`.
  */
 #define string_converter std::stof
+
+#ifdef MPI_VERSION
 /**
  * @def
  * @brief MPI version of `data_type`.
  */
 #define mpi_data_type MPI_FLOAT
+#endif
 
 #else
 /**
@@ -36,11 +39,14 @@
  * @brief A function used to convert from string to `data_type`.
  */
 #define string_converter std::stod
+
+#ifdef MPI_VERSION
 /**
  * @def
  * @brief MPI version of `data_type`.
  */
 #define mpi_data_type MPI_DOUBLE
+#endif
 
 #endif
 
