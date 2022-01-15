6b00dc274d20ed839352e6c01fb06d62dbf15d5e
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-01-15 11:05:07 +0100

    add progress bar

--- benchmark/generate_bench.py
+++ benchmark/generate_bench.py
@@ -1,8 +1,9 @@
 from random import gauss
 from csv import writer
 import sys
+from tqdm import tqdm
 
 with open(sys.argv[1], 'w') as csvfile:
 	wrt = writer(csvfile, delimiter=',')
-	for i in range(int(sys.argv[2])):
+	for i in tqdm(range(int(sys.argv[2]))):
 		wrt.writerow([str(gauss(0,10)) for _ in range(int(sys.argv[3]))])
